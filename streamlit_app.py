7557b4400c0f0b279342e349c9e6a2afae1d0e56
khan-of-mongol-empire <h098s02117@chunchon.gwe.hs.kr>
2025-11-10 12:33:23 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.set_page_config(
 
 # --- 2. 페이지 타이틀 ---
 st.title("Streamlit 마법 교실 🔮")
-st.subheader("춘#자신이 만든 레포지토리(저장소)에 streamlit_app.py 만들고 다음 내용 복붙해서 집어넣기
+st.subheader("춘천고등학교")
 
 import streamlit as st
 import pandas as pd
